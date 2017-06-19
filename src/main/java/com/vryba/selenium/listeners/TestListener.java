958b743e6285b01878a39e7189685f880c709577
Vladimir <vova.rybalko@gmail.com>
2017-06-18 20:00:05 -0400

    Upgraded Maven ver to 3.5.0

--- src/main/java/com/vryba/selenium/listeners/TestListener.java
+++ src/main/java/com/vryba/selenium/listeners/TestListener.java
@@ -7,44 +7,43 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
-
 import java.io.File;
 
 public class TestListener extends TestListenerAdapter {
     WebDriver driver;
-    private static String fileSeperator = System.getProperty("file.separator");
+    private static String fileSeparator = System.getProperty("file.separator");
 
     @Override
     public void onTestFailure(ITestResult result) {
         System.out.println("***** Error " + result.getName() + " test has failed *****");
 
-       driver = BrowserFactory.getWebDriver();
+        driver = BrowserFactory.getWebDriver();
 
         String testClassName = getTestClassName(result.getInstanceName()).trim();
-
         String testMethodName = result.getName().toString().trim() + "_%d.png";
         String screenShotName = String.format(testMethodName, System.currentTimeMillis());
 
         if (driver != null) {
-            String imagePath = ".." + fileSeperator + "screenshots"
-                    + fileSeperator + "results" + fileSeperator + testClassName
-                    + fileSeperator
+            String imagePath = ".." + fileSeparator + "screenshots"
+                    + fileSeparator + "results" + fileSeparator + testClassName
+                    + fileSeparator
                     + takeScreenShot(driver, screenShotName, testClassName);
-            System.out.println("Screenshot can be found : " + imagePath);
+            System.out.println("Screenshot can be found: " + imagePath);
         }
     }
 
     public static String takeScreenShot(WebDriver driver,
                                         String screenShotName, String testName) {
         try {
-            File file = new File("screenshots" + fileSeperator + "results");
+            File file = new File("screenshots" + fileSeparator + "results");
             if (!file.exists()) {
                 System.out.println("File created " + file);
                 file.mkdir();
             }
 
             File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-            File targetFile = new File("screenshots" + fileSeperator + "results" + fileSeperator + testName, screenShotName);
+            File targetFile = new File("screenshots" + fileSeparator + "results"
+                    + fileSeparator + testName, screenShotName);
             FileUtils.copyFile(screenshotFile, targetFile);
 
             return screenShotName;
@@ -57,7 +56,7 @@ public class TestListener extends TestListenerAdapter {
     public String getTestClassName(String testName) {
         String[] reqTestClassname = testName.split("\\.");
         int i = reqTestClassname.length - 1;
-        System.out.println("Required Test Name : " + reqTestClassname[i]);
+        System.out.println("Required Test Name: " + reqTestClassname[i]);
         return reqTestClassname[i];
     }
 
