9ac1ab06dee87b0a2b337ecd25a0d97858946dc5
Vladimir <vova.rybalko@gmail.com>
2017-07-03 16:26:21 -0400

    ;s replace @BeforeMethod to @BeforeClass annotation to keep the browser opened while executing all the @Test under target Test Case

--- src/main/java/com/vryba/selenium/utilities/TestBase.java
+++ src/main/java/com/vryba/selenium/utilities/TestBase.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 import static org.testng.Assert.fail;
@@ -14,7 +15,7 @@ public class TestBase {
     private StringBuffer verificationErrors = new StringBuffer();
     private Logger LOG = LogManager.getFormatterLogger();
 
-    @BeforeMethod
+    @BeforeClass
     public void methodSetup() {
         BrowserFactory.startBrowser(BrowserFactory.BrowserType.FirefoxDriver, baseUrl);
         LOG.info("Starting Browser");
