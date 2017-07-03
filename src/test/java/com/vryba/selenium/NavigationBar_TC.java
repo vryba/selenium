f8432ba9199e3327f9250aa0d97bb6d72bc246fc
Vladimir <vova.rybalko@gmail.com>
2017-07-03 16:38:22 -0400

    added test case to verify that the questions tab becomes active when user is navigation to Questions page

--- src/test/java/com/vryba/selenium/NavigationBar_TC.java
+++ src/test/java/com/vryba/selenium/NavigationBar_TC.java
@@ -11,11 +11,11 @@ import org.testng.annotations.Test;
 
 public class NavigationBar_TC extends TestBase{
     private Logger LOG = LogManager.getLogger(NavigationBar_TC.class);
+
     @Test
     public void checkActiveNavBarListItem() {
         LOG.info("Start Test: checkActiveNavBarListItem");
         NavigationBar navbar = new NavigationBar();
-        Assert.assertEquals(navbar.questionsNavBarListItem.getAttribute("class"), "-item");
         navbar.questionsNavBarListItem.click();
         Assert.assertEquals(navbar.questionsNavBarListItem.getAttribute("class"), "-item _current");
         LOG.info("Questions tab is active");
